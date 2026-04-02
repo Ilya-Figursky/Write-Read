4c3fd463f6a06f103da3bd7de6c6542dbd3d6fbb
Ilya Figursky <ilya.figursky@gmail.com>
2026-04-02 10:12:54 +0300

    Add 'IPostRepository' and 'PostRepository' into 'Repository'

--- backend/Persistence/obj/Debug/net10.0/Persistence.AssemblyInfo.cs
+++ backend/Persistence/obj/Debug/net10.0/Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d60391e23b50400b42be4cd841fb102a8fc2aa95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e4d0e1f9e5323c94a8e3e4527c9d5fbf85be817")]
 [assembly: System.Reflection.AssemblyProductAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
