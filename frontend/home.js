145fa60a20039ca4082924e71ef7374f7dd4a5ba
Ilya Figursky <ilya.figursky@gmail.com>
2026-05-17 21:37:07 +0300

    Frontend: add: 'postForm.html', 'postForm.js'; rename 'guest.js' into 'register.js'. In 'postForm.html' add post form and send button

--- frontend/home.js
+++ frontend/home.js
@@ -4,11 +4,45 @@ function checkAuth()
 
     if(userId) 
     {
-        //call method
+        loadPosts();
     }
+
+    console.log("Authorization error");
 }
 
 
+async function loadPosts()
+{
+    try
+    {
+        const response = await fetch("https://localhost:7109/wr/home");
+        
+        if(!response.ok) throw new Error("Server error");
+        
+        const posts = await response.json();
+
+        const postFeed = document.getElementById("postFeed");
+    
+        postFeed.innerHTML = "";
+
+        posts.forEach( post => {
+            
+        const postBlok = document.createElement("div");
+
+        postBlok.innerHTML = `
+            <p>${post.content}</p>
+            <hr>
+        `;
+
+            postFeed.appendChild(postBlok);
+        });
+    } catch(error)
+    {   
+        console.log(error);
+
+        document.getElementById("postFeed").innerHTML = "The \"posts\" not found"
+    }
+}
 
 
 window.onload = checkAuth;
